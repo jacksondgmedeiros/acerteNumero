914cef32df1a420747697c27a8a68e406e12d78c
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-10-01 18:35:22 -0300

    projeto de criação do número secreto, nesse commit foi criado a mensagem de boas vindas, variáveis para armazenar o número secreto fixo e o chute do usuário, também criei um if para verificar se o usuário acertou o número secreto. No final do módulo, realizei os desafios que foram propostos, eles estão na pasra desafios.

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Boas vindas ao jogo do número secreto!");
+
+let numeroSecreto = 29;
+
+let chute = prompt("Escolha um número entre 1 e 10");
+
+if (numeroSecreto === chute) {
+    console.log("Você descobriu o número secreto!");
+}
