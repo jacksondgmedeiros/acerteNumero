6bf2c0329b19a25f5bf904ffcdf569753fec87c0
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-10-03 21:01:17 -0300

    adicionando else na condição para que ele mostre se acertou ou errou o número

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let numeroSecreto = 29;
 
 let chute = prompt("Escolha um número entre 1 e 10");
 
-if (numeroSecreto === chute) {
-    console.log("Você descobriu o número secreto!");
+if (numeroSecreto == chute) {
+   alert(`Você descobriu o número secreto! ${numeroSecreto}`);
+} else {
+    alert("Você errou!");
 }
