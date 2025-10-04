4d713852d80a6babbacada6d21395f78bb50f2ab
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-10-03 22:20:33 -0300

    projeto finalizado com o número aleatório sendo realizado pelo código, adicionando um break para não executar mais nenhum código depois e usando um operador ternário para a palavra tentativa ficar de acordo com a lingua portuguesa

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 alert("Boas vindas ao jogo do número secreto!");
+let numeroMaximo = 5000;
 
-let numeroSecreto = 8;
+// cria um número aleatório usando a função do Math.ramdom() e transforma em um inteiro, depois multiplica pelo valor do numero maximo e usa o mais um para arredondar para cima
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 
 let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = prompt("Escolha um número entre 1 e 10");
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
     if (numeroSecreto == chute) {
-        alert(`Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativas.`);
+       break;
     } else {
         if(chute > numeroSecreto){
             alert(`O número é menor que ${chute}`);
@@ -19,3 +21,6 @@ while (chute != numeroSecreto) {
     }
     tentativas++;
 }
+// operador ternario, primeiro é o verdadeiro e depois o falso
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
